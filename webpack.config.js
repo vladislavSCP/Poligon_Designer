635bb9f6abd33bba1e89f2b762f9f44e700d50a3
AleksandrPistoletov <145144668+AleksandrPistoletov@users.noreply.github.com>
2025-07-17 13:44:02 +0300

    fix: remove manual main.js script tag

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
 export default {
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
